ea08ca61e5b71f9c5cf3fb5591461cd031eef513
stopachka <stepan.p@gmail.com>
2025-04-04 13:52:10 -0700

    roar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,52 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { init, i, InstaQLEntity } from "@instantdb/react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+// ID for app: rn-g-signin
+const APP_ID = "6e5ab9aa-ce41-4b50-a1fd-260848fdfd3e";
 
-export default function HomeScreen() {
+const db = init({ appId: APP_ID });
+
+GoogleSignin.configure({
+  scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  webClientId:
+    "568313895998-c041sggh5hnst2tg018m0igi6vpvmpjm.apps.googleusercontent.com",
+});
+
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <GoogleSigninButton
+      size={GoogleSigninButton.Size.Wide}
+      color={GoogleSigninButton.Color.Dark}
+      onPress={async () => {
+        // try {
+        //   await GoogleSignin.hasPlayServices();
+        //   const userInfo = await GoogleSignin.signIn();
+        //   if (userInfo.data.idToken) {
+        //     const { data, error } = await supabase.auth.signInWithIdToken({
+        //       provider: "google",
+        //       token: userInfo.data.idToken,
+        //     });
+        //     console.log(error, data);
+        //   } else {
+        //     throw new Error("no ID token present!");
+        //   }
+        // } catch (error: any) {
+        //   if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        //     // user cancelled the login flow
+        //   } else if (error.code === statusCodes.IN_PROGRESS) {
+        //     // operation (e.g. sign in) is in progress already
+        //   } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        //     // play services not available or outdated
+        //   } else {
+        //     // some other error happened
+        //   }
+        // }
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
